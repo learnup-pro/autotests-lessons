ee1adec4dfb55a2b0cfd504e03fe7ae502a2fa0f
dimakar <makarovskii@phystech.edu>
2022-01-30 20:03:06 +0300

    lesson 1

--- src/test/java/pro/learnup/ShopTest.java
+++ src/test/java/pro/learnup/ShopTest.java
@@ -1,21 +1,56 @@
 package pro.learnup;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ShopTest extends BaseTest {
+public class ShopTest {
+
+    protected WebDriver webDriver;
+    protected WebDriverWait webDriverWait;
+
+    protected void login() {
+        webDriver.get("http://localhost:3000/");
+
+        webDriver.findElement(By.xpath("//button[.='LOGIN']")).click();
+        webDriver.findElement(By.xpath("//input[contains(@id, 'Username')]")).sendKeys("admin");
+        webDriver.findElement(By.xpath("//input[contains(@id, 'Password')]")).sendKeys("admin");
+        webDriver.findElement(By.xpath("//button[.='Submit']")).click();
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[text()='LOGOUT']")));
+    }
+
+    @BeforeEach
+    void setUp() {
+
+//        ChromeOptions chromeOptions = new ChromeOptions().addArguments("--blink-settings=imagesEnabled=false");
+        webDriver = WebDriverManager.chromedriver().create();
+//        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+//        webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        webDriverWait = new WebDriverWait(webDriver, 3);
+    }
+
+    @AfterEach
+    void tearDown() {
+        webDriver.quit();
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {"HTC U11"})
-    public void buyPhoneTest(String phoneName) throws InterruptedException {
+    public void buyPhoneTest(String phoneName) {
 
         login();
 
